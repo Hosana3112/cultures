7a98f7acd04541f83c4395fce638fba08f3547d4
Hosana <zittihosana@gmail.com>
2025-12-08 19:16:17 +0100

    deploiement preparation

--- routes/admin.php
+++ routes/admin.php
@@ -5,32 +5,32 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->middleware(['auth'])->group(function(){
     // Routes avec préfixe 'admin.' pour éviter les conflits
     Route::resource('langues', 'App\Http\Controllers\LanguesController')
-        ->names('admin.langues');
+        ->names('langues');
     
     Route::put('contenus/{contenu}/valider', ['App\Http\Controllers\ContenusController', 'valider'])
-        ->name('admin.contenus.valider');
+        ->name('contenus.valider');
     
     Route::resource('contenus', 'App\Http\Controllers\ContenusController')
-        ->names('admin.contenus');
+        ->names('contenus');
     
     Route::resource('roles', 'App\Http\Controllers\RolesController')
-        ->names('admin.roles');
+        ->names('roles');
     
     Route::resource('utilisateurs', 'App\Http\Controllers\UtilisateursController')
-        ->names('admin.utilisateurs');
+        ->names('utilisateurs');
     
     Route::resource('medias', 'App\Http\Controllers\MediasController')
-        ->names('admin.medias');
+        ->names('medias');
     
     Route::resource('typecontenus', 'App\Http\Controllers\TypeContenuesController')
-        ->names('admin.typecontenus');
+        ->names('typecontenus');
     
     Route::resource('typemedias', 'App\Http\Controllers\TypeMediasController')
-        ->names('admin.typemedias');
+        ->names('typemedias');
     
     Route::resource('regions', 'App\Http\Controllers\RegionsController')
-        ->names('admin.regions');
+        ->names('regions');
     
     Route::resource('commentaires', 'App\Http\Controllers\CommentairesController')
-        ->names('admin.commentaires');
+        ->names('commentaires');
 });
